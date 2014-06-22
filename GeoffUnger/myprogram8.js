27f1d6ad1c161a9a2deb7dd449f45c8d8b7f1c4d
Geoffrey Unger <geoffunger@gmail.com>
2014-06-22 15:48:30 -0700

    Problem 8 done

--- GeoffUnger/myprogram8.js
+++ GeoffUnger/myprogram8.js
@@ -1,17 +1,19 @@
+
 var http = require('http');
 
 var url = process.argv[2];
 
 var returnString = "";
 
-var req = http.request(url, function(res){
-    res.on('data', function(chunk){
+var req = http.request(url, function (res) {
+    res.on('data', function (chunk) {
         returnString += chunk.toString();
         //console.log(returnString);
     })
-    console.log(returnString.length);
-    console.log(returnString);
+    res.on('end', function () {
+        console.log(returnString.length);
+        console.log(returnString);
+    })
 
-})
-req.end();
+    }).end();
 
