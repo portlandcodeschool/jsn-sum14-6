96b61a80a04b06983cc778d6fa7f3b1aed3b2ad0
Tom McCluskey <bosun.tom@gmail.com>
2014-06-22 16:28:38 -0700

    nearly done with recipe box; final formatting remains

--- tom/recipe-instructions.js
+++ tom/recipe-instructions.js
@@ -10,9 +10,16 @@
 
 //! bring in the fs module here !
 
+var fs = require('fs');
 var recipeBox = {};
 
-recipeBox.readRecipeData = function (file, callback) {
+recipeBox.readRecipeData = function (filename, callback) {
+  fs.readFile(filename, {encoding: 'utf8'}, function( err, data ) {
+    if (err) { throw err ;}
+    //console.log(data);
+    callback(null, JSON.parse(data));
+  });
+};
   //this function should:
   // 1. call a method from node that will read a file asynchronously (hint: fs module)
   //    - this function call should: 
@@ -27,14 +34,26 @@ recipeBox.readRecipeData = function (file, callback) {
   //     - pass in null for the first argument 
   //       (this means we are not passing an error object)
   //     - pass in the array of your entire data
-}
 
-recipeBox.printRecipeCards = function (error, data) {
+recipeBox.printRecipeCards = function (err, array) {
+  if (err) { throw err ;}
+  console.log("My Recipe Collection");
+  for (var i = 0; i < array.length; i++) {
+    console.log('\n\n' + array[i].title);
+    for (var j = 0; j < array[i].ingredients.length; j++) {
+      var ingredient = array[i].ingredients[j];
+      console.log("\t" + ingredient.amount + ' ' + ingredient.unit + ' of ' + ingredient.ingredient);
+    }
+    console.log();
+    for (var k = 0; k < array[i].directions.length; k++) {
+      console.log(k + 1  + '. ' + array[i].directions[k].direction);
+    }
     // this function should:
     // 1. Print the contents of data such that they look something like
     //    output-example.jpg
     // 2. if there is an error throw one
-}
+  }
+};
 
 recipeBox.readRecipeData('./recipes.json', recipeBox.printRecipeCards);
 // this function call and the options (arguments) passed in should 
