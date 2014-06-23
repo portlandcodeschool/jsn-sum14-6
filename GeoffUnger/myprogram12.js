29ffb26f4cef5cadb2e8b5116f74d9a0b8a43da1
Geoffrey Unger <geoffunger@gmail.com>
2014-06-22 18:58:45 -0700

    Problem 12 done

--- GeoffUnger/myprogram12.js
+++ GeoffUnger/myprogram12.js
@@ -1,6 +1,22 @@
-/**
- * Created with PhpStorm.
- * User: geoffreyunger
- * Date: 6/22/14
- * Time: 5:52 PM
- */
+var fs = require("fs");
+var http = require("http");
+var map = require("through2-map");
+var port = process.argv[2];
+var file = process.argv[3];
+
+
+var callback = function(request, response){
+
+    if(request.method == "POST"){
+        request.pipe(map(function (chunk) {
+
+            return chunk.toString().toUpperCase();
+
+        })).pipe(response);
+    } else {response.end();}
+
+}
+
+var server = http.createServer(callback);
+
+server.listen(port);
