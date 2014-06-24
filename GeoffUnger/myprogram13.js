44a828ca92b9a5d537ba91bb81bef6d4c80ad244
Geoffrey Unger <geoffunger@gmail.com>
2014-06-23 20:59:49 -0700

    Added some routes to myprogram13.js for fun

--- GeoffUnger/myprogram13.js
+++ GeoffUnger/myprogram13.js
@@ -2,8 +2,9 @@ var http = require("http");
 var url = require("url");
 
 var callback = function(request, response){
+    //console.log(request);
     var parsedURL = url.parse(request.url, true);
-    console.log(parsedURL);
+    //console.log(parsedURL);
     var route = parsedURL.pathname;
     var query = parsedURL.query;
     var date = new Date(query.iso);
@@ -18,11 +19,25 @@ var callback = function(request, response){
             response.end();
             break;
         }
+        case "/api/sayhi":{
+            response.write("Hello there " + query.name + "!");
+            console.log(query.name);
+            response.end();
+        }
         case "/api/unixtime":{
             response.writeHead(200, { 'Content-Type': 'application/json' });
             response.write(JSON.stringify({unixtime:date.getTime()}));
             response.end();
             break;
+        }
+        case "/":{
+            response.writeHead(200, {'Content-Type':'text/html'});
+            response.write("<marquee><h1>HOME PAGEEEEEE!!!</h1></marquee>");
+            response.end();
+        }
+        default :{
+            response.writeHead(404, {'Content-Type':'text/html'});
+            response.write("<h1>NOTHING FOUND</h1>");
         }
     }
 }
