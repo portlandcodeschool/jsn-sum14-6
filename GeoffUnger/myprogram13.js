4babf27a936091bb009e5f3ec05f4161dc519cf2
Geoffrey Unger <geoffunger@gmail.com>
2014-06-23 18:33:36 -0700

    Problem 13 done

--- GeoffUnger/myprogram13.js
+++ GeoffUnger/myprogram13.js
@@ -2,8 +2,27 @@ var http = require("http");
 var url = require("url");
 
 var callback = function(request, response){
-    parsedURL = url.parse(request);
-    //route = parsedURL.path TODO continue from here
+    var parsedURL = url.parse(request.url, true);
+    console.log(parsedURL);
+    var route = parsedURL.pathname;
+    var query = parsedURL.query;
+    var date = new Date(query.iso);
+    switch(route){
+        case "/api/parsetime":{
+            var timeObj = {};
+            timeObj.hour = date.getHours();
+            timeObj.minute = date.getMinutes();
+            timeObj.second = date.getSeconds();
+            response.write(JSON.stringify(timeObj));
+            response.end();
+            break;
+        }
+        case "/api/unixtime":{
+            response.write(JSON.stringify({unixtime:date.getTime()}));
+            response.end();
+            break;
+        }
+    }
 }
 
 var server = http.createServer(callback);
